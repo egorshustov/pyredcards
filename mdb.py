64b76610f7f010279542fd36a617a46b356caa86
Egor Shustov <egorshustov.93@gmail.com>
2018-10-27 07:51:57 +0500

    class array is created and initialized by mdb data

--- mdb.py
+++ mdb.py
@@ -1,13 +1,21 @@
 
 import csv, pyodbc
 from pyodbc import *
-db = connect('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=.\\RC_base.mdb') #order.mdb - собственно мое файло БД
+
+class League:
+    def __init__(self, league_name, url_whoscored, url_championat):
+        self.league_name = league_name
+        self.url_whoscored = url_whoscored
+        self.url_championat = url_championat
+
+db = connect('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=.\\RC_base.mdb')
 dbc = db.cursor()
 rows = dbc.execute('select * from [Leagues]').fetchall()
 db.close()
 
 
-# you could change the mode from 'w' to 'a' (append) for any subsequent queries
-with open('report.csv', 'w') as fou:
-    csv_writer = csv.writer(fou) # default field-delimiter is ","
-    csv_writer.writerows(rows)
+league_length = len(rows)
+league = []
+for i in range(0,league_length):
+    league.append(League(rows[i][1],rows[i][2],rows[i][3]))
+
