d11d761fb0c34764cffa2f1c5751f546e3728788
Egor Shustov <egorshustov.93@gmail.com>
2018-10-28 09:03:51 +0500

    function write_to_spreadsheets created

--- mdb.py
+++ mdb.py
@@ -8,6 +8,27 @@ class League:
         self.url_whoscored = url_whoscored
         self.url_championat = url_championat
 
+def write_to_spreadsheets():
+    # Создаём Service-объект, для работы с Google-таблицами:
+    CREDENTIALS_FILE = 'RedCardsProject-90325d995892.json'  # имя выгруженного файла с закрытым ключом
+    # В Scope укажем к каким API мы хотим получить доступ:
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    # Заполним массив с учётными данными:
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, scope)
+    # Авторизуемся с этими учётными данными:
+    gc = gspread.authorize(credentials)
+    # Откроем Spreadsheet с указанным именем:
+    sh = gc.open('Python')
+    # Получим первый лист этого Spreadsheet:
+    worksheet = sh.sheet1
+    # удалить лист из файла (удалится только если лист не единственный):
+    #sh.del_worksheet(worksheet)
+    # предоставить себе роль владельца файла:
+    # sh.share('egorshustov.93@gmail.com', perm_type='user', role='owner')
+    # Запишем каждую строку списка league[] в лист worksheet:
+    for i in range(0,league_length):
+        worksheet.append_row([league[i].league_name,league[i].url_whoscored,league[i].url_championat])
+
 # Подключимся к БД Microsoft Access через экземпляр ODBC
 db = connect('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=.\\RC_base.mdb')
 dbc = db.cursor()
@@ -22,22 +43,4 @@ league = []
 for i in range(0,league_length):
     league.append(League(rows[i][1],rows[i][2],rows[i][3]))
 
-# Создаём Service-объект, для работы с Google-таблицами:
-CREDENTIALS_FILE = 'RedCardsProject-90325d995892.json'  # имя выгруженного файла с закрытым ключом
-# В Scope укажем к каким API мы хотим получить доступ:
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-# Заполним массив с учётными данными:
-credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, scope)
-# Авторизуемся с этими учётными данными:
-gc = gspread.authorize(credentials)
-# Откроем Spreadsheet с указанным именем:
-sh = gc.open('Python')
-# Получим первый лист этого Spreadsheet:
-worksheet = sh.sheet1
-# удалить лист из файла (удалится только если лист не единственный):
-#sh.del_worksheet(worksheet)
-# предоставить себе роль владельца файла:
-# sh.share('egorshustov.93@gmail.com', perm_type='user', role='owner')
-# Запишем каждую строку списка league[] в лист worksheet:
-for i in range(0,league_length):
-    worksheet.append_row([league[i].league_name,league[i].url_whoscored,league[i].url_championat])
+write_to_spreadsheets()
