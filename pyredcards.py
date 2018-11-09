0e2c6969e8a4c60acf2e57c6ad349c548015d10f
Egor Shustov <egorshustov.93@gmail.com>
2018-11-09 11:29:54 +0500

    started to write matches data to Google Sheets using API v4, some bugs fixed

--- pyredcards.py
+++ pyredcards.py
@@ -70,49 +70,49 @@ class League:
         self.league_name = league_name
         self.url_whoscored = url_whoscored
         self.url_championat = url_championat
-        self.url_past_season = ""
-        self.url_referee_statistics = ""
+        self.url_past_season = ''
+        self.url_referee_statistics = ''
         self.matches_found = True
 
 
 class Match:
 
     def __init__(self):
-        self.team_home_url = ""
-        self.team_away_url = ""
-        self.match_url = ""  # ссылка на личную встречу команд
-        self.league_name = ""  # Лига
-        self.team_home_name = ""  # Дома
-        self.team_away_name = ""  # Гости
-        self.match_datetime = ""  # Дата
-        self.referee_name_championat = ""  # Судья, Имя на championat
-        self.referee_name_championat_translated_to_en = ""
-        self.referee_name_whoscored = ""  # Судья, Имя на whoscored
-        self.referee_url = ""  # Судья, URL на whoscored
-        self.referee_this_season_average = ""  # Судья, Этот сезон
+        self.team_home_url = ''
+        self.team_away_url = ''
+        self.match_url = ''  # ссылка на личную встречу команд
+        self.league_name = ''  # Лига
+        self.team_home_name = ''  # Дома
+        self.team_away_name = ''  # Гости
+        self.match_datetime = ''  # Дата
+        self.referee_name_championat = ''  # Судья, Имя на championat
+        self.referee_name_championat_translated_to_en = ''
+        self.referee_name_whoscored = ''  # Судья, Имя на whoscored
+        self.referee_url = ''  # Судья, URL на whoscored
+        self.referee_this_season_average = ''  # Судья, Этот сезон
         self.referee_this_season_matches_count = -1
-        self.referee_all_seasons_average = ""  # Судья, Все сезоны
+        self.referee_all_seasons_average = ''  # Судья, Все сезоны
         self.referee_all_seasons_matches_count = -1
-        self.referee_to_team_home_average = ""  # Судья, Командам
-        self.referee_to_team_away_average = ""
+        self.referee_to_team_home_average = ''  # Судья, Командам
+        self.referee_to_team_away_average = ''
         self.referee_last_twenty_home_count = 0  # Судья, Посл. 20 игр
         self.referee_last_twenty_away_count = 0
-        self.referee_last_twenty_last_kk_date = ""
+        self.referee_last_twenty_last_kk_date = ''
         self.team_home_kk_this_season_count = 0  # Команды, КК этот сезон
         self.team_away_kk_this_season_count = 0
         self.team_home_found_in_last_season = False  # Команды, КК предыдущий сезон
         self.team_home_kk_last_season_count = 0
         self.team_away_found_in_last_season = False
         self.team_away_kk_last_season_count = 0
-        self.team_home_last_kk_date = ""  # Команды, Дата посл 1
-        self.team_away_last_kk_date = ""  # Команды, Дата посл 2
+        self.team_home_last_kk_date = ''  # Команды, Дата посл 1
+        self.team_away_last_kk_date = ''  # Команды, Дата посл 2
         self.team_home_personal_meetings_kk_count_home = 0  # Команды, Личн. вст
         self.team_home_personal_meetings_kk_count_away = 0
         self.team_away_personal_meetings_kk_count_home = 0
         self.team_away_personal_meetings_kk_count_away = 0
         self.personal_meetings_count = 0
-        self.teams_personal_meetings_last_kk_date = ""
-        self.teamsstring = ""
+        self.teams_personal_meetings_last_kk_date = ''
+        self.teamsstring = ''
 
 
 def datestring_to_unix(datestring):
@@ -147,13 +147,13 @@ def get_matches():
             if required_date in tournament_fixture_innerhtml:
                 # Если искомая дата присутствует в таблице, спарсим все матчи на искомую дату для данной лиги.
                 # Спарсим все дни с матчами в таблице:
-                days = tournament_fixture_innerhtml.split('<tr class=\"rowgroupheader\"><th colspan=\"7\">')
+                days = tournament_fixture_innerhtml.split('<tr class="rowgroupheader"><th colspan="7">')
                 # Пройдёмся по всем дням в таблице и найдём тот день, в котором присуствует искомая дата:
                 for day in days:
                     if required_date in day:
                         # Это искомый день. Спарсим всю информацию по матчам для искомого дня
                         # данной лиги и выйдем из цикла:
-                        soup = BeautifulSoup(day, "html.parser")
+                        soup = BeautifulSoup(day, 'html.parser')
                         match_datetime = soup.findAll('td', {'class': 'time'})
                         teams_home = soup.findAll('td', {'class': 'team home'})
                         teams_away = soup.findAll('td', {'class': 'team away'})
@@ -162,7 +162,7 @@ def get_matches():
                         for j in range(0, len(teams_home)):
                             match.append(Match())
                             match[i_match].league_name = league[i].league_name
-                            match[i_match].match_datetime = required_date + " " + match_datetime[j].text
+                            match[i_match].match_datetime = required_date + ' ' + match_datetime[j].text
                             match[i_match].team_home_url = 'https://ru.whoscored.com' + \
                                                            teams_home[j].find('a', {'class': 'team-link '})['href']
                             match[i_match].team_home_name = teams_home[j].find('a', {'class': 'team-link '}).text
@@ -176,8 +176,10 @@ def get_matches():
             else:
                 rowgroupheaders = tournament_fixture.find_elements_by_class_name('rowgroupheader')
                 # Возьмём последний rowgroupheader в таблице и достанем из него дату:
-                # (также уберём из его innerText последний символ (символ перевода строки через операцию среза [0:-1]):
-                last_date_in_table = rowgroupheaders[-1].get_property('innerText')[0:-1]
+                # (также уберём из его innerText символ перевода строки:
+                last_date_in_table = rowgroupheaders[-1].get_property('innerText')
+                last_date_in_table = last_date_in_table.replace('\n', '')
+
                 last_date_in_table_unix = datestring_to_unix(last_date_in_table)
                 if last_date_in_table_unix > required_date_unix:
                     # Если последний день в текущем диапазоне дней больше указанного
@@ -214,12 +216,12 @@ def get_personal_meetengs():
         # по тексту заголовка previous-meetings-count
         previous_meetings_count = ui.WebDriverWait(driver, 15).until(
             lambda driver1: driver.find_element_by_id('previous-meetings-count')).get_property('innerText')
-        # Если текст заголовка previous-meetings-count не пустой (например, "(Последние N матчей)")
+        # Если текст заголовка previous-meetings-count не пустой (например, '(Последние N матчей)')
         if previous_meetings_count != '':
             # Получим таблицу предыдущих встреч двух команд:
             previous_meetings_grid = ui.WebDriverWait(driver, 15).until(
                 lambda driver1: driver.find_element_by_id('previous-meetings-grid')).get_property('innerHTML')
-            soup = BeautifulSoup(previous_meetings_grid, "html.parser")
+            soup = BeautifulSoup(previous_meetings_grid, 'html.parser')
             # Получим все предыдущие встречи двух команд и занесём их в список:
             previous_matches = soup.findAll('tr', {'class': 'item'})
             # Определим количество этих предыдущих матчей:
@@ -270,18 +272,186 @@ def write_to_spreadsheets():
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     # Заполним массив с учётными данными:
     credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials_file, scope)
-    httpAuth = credentials.authorize(httplib2.Http())
+    http_auth = credentials.authorize(httplib2.Http())
     # Создаём Service-объект для работы с Google-таблицами:
-    service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
+    service = apiclient.discovery.build('sheets', 'v4', http=http_auth)
     # Укажем идентификатор документа, к которому хотим получить доступ.
     spreadsheet_id = '10PPb2Tk51-68fBqew-tEnDqbA_MaCEQGyrHAxcnQ4Jc'
-    # The ranges to retrieve from the spreadsheet.
     ranges = []
-    # True if grid data should be returned.
-    # This parameter is ignored if a field mask was set in the request.
-    include_grid_data = False  # TODO: Update placeholder value.
+    include_grid_data = False
     request = service.spreadsheets().get(spreadsheetId=spreadsheet_id, ranges=ranges, includeGridData=include_grid_data)
     spreadsheet = request.execute()
+
+    # Прочитаем первые две строки листа:
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheet['spreadsheetId'], range='Лист1!A1:O2').execute()
+    num_rows = result.get('values') if result.get('values') is not None else 0
+    # Если первые две строки листа пустые, то сделаем заголовок:
+    if num_rows == 0:
+        service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheet['spreadsheetId'], body={
+            'valueInputOption': 'USER_ENTERED',
+            'data': [
+                {
+                    'range': 'Лист1!A1:O2',
+                    'majorDimension': 'ROWS',
+                    # сначала заполнять ряды, затем столбцы (т.е. самые внутренние списки в values - это ряды)
+                    'values':
+                    [
+                        ['Лига', 'Дома', 'Гости', 'Дата', 'Судья', '', '', '', 'Команды', '', '', '', '', '', 'Судья'],
+                        ['', '', '', '', 'Этот сезон', 'Все сезоны', 'Командам', 'Посл. 20 игр', 'КК этот сезон',
+                         'КК прош. сезон', 'Дата последней 1/2', '', 'Личные встречи', '',
+                         'Имя на Championat (имя на Whoscored)']
+                    ]
+                }
+            ]
+        }).execute()
+        # Определим объект границы (тип данных словарь), чтобы применять его при рисовании границ:
+        border = {
+            'style': 'SOLID', 'width': 3,
+            'color':
+                {
+                    'red': 0, 'green': 0, 'blue': 0, 'alpha': 1
+                }
+        }
+        service.spreadsheets().batchUpdate(spreadsheetId=spreadsheet['spreadsheetId'], body={
+            'requests': [
+                # Нарисуем границы для всех ячеек:
+                {
+                    'updateBorders': {
+                        'range': {
+                            'sheetId': 0,
+                            'startRowIndex': 0,
+                            'endRowIndex': 2,
+                            'startColumnIndex': 0,
+                            'endColumnIndex': 15
+                        },
+                        'top': border,
+                        'bottom': border,
+                        'left': border,
+                        'right': border,
+                        'innerHorizontal': border,
+                        'innerVertical': border
+                    }
+                },
+                # Удалим границы для ячеек столбца N:
+                {
+                    'updateBorders': {
+                        'range': {
+                            'sheetId': 0,
+                            'startRowIndex': 0,
+                            'endRowIndex': 2,
+                            'startColumnIndex': 13,
+                            'endColumnIndex': 14
+                        },
+                        'top': {'style': 'NONE'},
+                        'bottom': {'style': 'NONE'},
+                        'innerHorizontal': {'style': 'NONE'}
+                    }
+                },
+                # Выполним слияние необходимых ячеек в заголовке таблицы:
+                {
+                    'mergeCells': {
+                        'range': {
+                            'sheetId': 0,
+                            'startRowIndex': 0,
+                            'endRowIndex': 1,
+                            'startColumnIndex': 4,
+                            'endColumnIndex': 8
+                        },
+                        'mergeType': 'MERGE_ALL'
+                    }
+                },
+                {
+                    'mergeCells': {
+                        'range': {
+                            'sheetId': 0,
+                            'startRowIndex': 0,
+                            'endRowIndex': 1,
+                            'startColumnIndex': 8,
+                            'endColumnIndex': 13
+                        },
+                        'mergeType': 'MERGE_ALL'
+                    }
+                },
+                {
+                    'mergeCells': {
+                        'range': {
+                            'sheetId': 0,
+                            'startRowIndex': 1,
+                            'endRowIndex': 2,
+                            'startColumnIndex': 0,
+                            'endColumnIndex': 4
+                        },
+                        'mergeType': 'MERGE_ALL'
+                    }
+                },
+                {
+                    'mergeCells': {
+                        'range': {
+                            'sheetId': 0,
+                            'startRowIndex': 1,
+                            'endRowIndex': 2,
+                            'startColumnIndex': 10,
+                            'endColumnIndex': 12
+                        },
+                        'mergeType': 'MERGE_ALL'
+                    }
+                }
+            ]
+        }).execute()
+
+    # Выведем все матчи в цикле:
+    for i in range(0, matches_length):
+        # Соберём строку str_personal_meetings для её последующего вывода:
+        if ((match[i].team_home_personal_meetings_kk_count_home != -1) and
+                (match[i].team_home_personal_meetings_kk_count_away != -1) and
+                (match[i].team_away_personal_meetings_kk_count_home != -1) and
+                (match[i].team_away_personal_meetings_kk_count_away != -1)):
+
+                str_personal_meetings = str(match[i].team_home_personal_meetings_kk_count_home) + 'д' +\
+                                       str(match[i].team_home_personal_meetings_kk_count_away) + 'г/' +\
+                                       str(match[i].team_away_personal_meetings_kk_count_home) + 'д' +\
+                                       str(match[i].team_away_personal_meetings_kk_count_away) + 'г из ' +\
+                                       str(match[i].personal_meetings_count) + ' (' +\
+                                       match[i].teams_personal_meetings_last_kk_date + ')'
+
+        else:
+                str_personal_meetings = "Не встречались"
+        # Выведем строку матча:
+        service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheet['spreadsheetId'], body={
+            'valueInputOption': 'USER_ENTERED',
+            'data': [
+                {
+                    'range': 'Лист1!A'+str(i+3)+':O'+str(i+3)+'',
+                    'majorDimension': 'ROWS',
+                    # сначала заполнять ряды, затем столбцы (т.е. самые внутренние списки в values - это ряды)
+                    'values':
+                        [
+                            # ['Лига', 'Дома', 'Гости', 'Дата', 'Судья', '', '', '', 'Команды', '', '', '', '', '', 'Судья']
+                            [match[i].league_name, match[i].team_home_name, match[i].team_away_name,
+                             '', '', '', '', '', '', '', '', '',
+                             str_personal_meetings,
+                             '', '']
+                        ]
+                }
+            ]
+        }).execute()
+
+    service.spreadsheets().batchUpdate(spreadsheetId=spreadsheet['spreadsheetId'], body={
+        'requests': [
+            # Задать размер по содержимому для всех столбцов
+            {
+                'autoResizeDimensions': {
+                    'dimensions': {
+                        'sheetId': 0,
+                        'dimension': 'COLUMNS',  # COLUMNS - потому что столбец
+                        'startIndex': 0,  # Столбцы нумеруются с нуля
+                        'endIndex': 15  # startIndex берётся включительно, endIndex - НЕ включительно
+                    }
+                }
+            }
+        ]
+    }).execute()
 
 
 def main():
@@ -299,8 +469,8 @@ def main():
     for i in range(0, league_length):
         league.append(League(rows[i][1], rows[i][2], rows[i][3]))
 
-    includes_path = "C:/Users/BRAXXMAN/PycharmProjects/includes/"
-    # os.environ["webdriver.chrome.driver"] = includes_path+"chromedriver.exe"
+    includes_path = 'C:/Users/BRAXXMAN/PycharmProjects/includes/'
+    # os.environ['webdriver.chrome.driver'] = includes_path+'chromedriver.exe'
     # driver = webdriver.Chrome(executable_path=includes_path+'chromedriver.exe')
     global driver
     driver = webdriver.Firefox(executable_path=includes_path + 'geckodriver.exe')
